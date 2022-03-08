eef09e1c92fc6c76b37f0077610e81724152bded
SQ <wsq2001@outlook.com>
2022-03-08 23:33:47 +0800

    SQ

--- src/main/java/com/sq/SYTreeHole/common/Result.java
+++ src/main/java/com/sq/SYTreeHole/common/Result.java
@@ -5,11 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Result<T> {
+public class Result<T> implements Serializable {
 
     private String code;
     private String msg;
