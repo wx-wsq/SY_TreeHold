2e957e6c1c467a0336d0b96d095c816ca1f19b93
SQ <wsq2001@outlook.com>
2022-03-08 23:34:27 +0800

    SQ

--- src/main/java/com/sq/SYTreeHole/dao/LoginMapper.java
+++ src/main/java/com/sq/SYTreeHole/dao/LoginMapper.java
@@ -2,8 +2,6 @@ package com.sq.SYTreeHole.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.sq.SYTreeHole.entity.User;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface LoginMapper extends BaseMapper<User> {
 }
