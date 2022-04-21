feedbc8ebdf52ba67641498ef35786785cbcd398
SQ <wsq2001@outlook.com>
2022-04-21 21:39:31 +0800

    SQ

--- src/main/java/com/sq/SYTreeHole/Interceptor/ErrorInterceptor.java
+++ src/main/java/com/sq/SYTreeHole/Interceptor/ErrorInterceptor.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.apache.catalina.connector.RequestFacade;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
 import javax.servlet.ServletRequestWrapper;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +21,7 @@ public class ErrorInterceptor implements HandlerInterceptor {
             else
                 ip = request.getHeader("x-forwarded-for");
             log.error("发生/error错误：来自于：{},访问的接口为：{}", ip, ((RequestFacade) ((ServletRequestWrapper) request).getRequest()).getRequestURI());
+            throw new Exception("此操作会被记录IP，请注意");
         }
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
@@ -27,7 +29,6 @@ public class ErrorInterceptor implements HandlerInterceptor {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
-        throw new Exception("此操作会被记录IP，请注意");
     }
 
     @Override
